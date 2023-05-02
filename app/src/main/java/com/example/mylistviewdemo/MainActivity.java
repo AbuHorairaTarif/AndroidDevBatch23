002cc7df56d81736abb9751649fb104cc879d035
= <=>
2023-05-02 16:09:36 +0600

    Second Commit

--- app/src/main/java/com/example/mylistviewdemo/MainActivity.java
+++ app/src/main/java/com/example/mylistviewdemo/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.mylistviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this,R.layout.sample_view,R.id.textViewId,countryNames);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = countryNames[position];
+                Toast.makeText(MainActivity.this,value+" "+position,Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
